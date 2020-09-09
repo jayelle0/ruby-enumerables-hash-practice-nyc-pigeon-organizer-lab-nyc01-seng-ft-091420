4821ae0d71e1d88f28e9309a21450a98819f040b
Jacqueline <lojacqueline2@gmail.com>
2020-09-09 00:06:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results =data.each_with_object({}) do |(key, value), final_array|
@@ -11,6 +13,8 @@ def nyc_pigeon_organizer(data)
         end
         final_array[name][key].push(inner_key.to_s)
      end   
+     
+     binding.pry
 end
 end
 end
